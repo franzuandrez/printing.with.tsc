fc3f851cdd7d058a703e5f26eecd38b04e099d77
franzua <edson.andrez@outlook.es>
2021-11-30 17:29:13 -0600

    PrintoutModel added

---
+++ Models/PrintoutModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSCLIB_DLL_IN_C_Sharp.Models
+{
+    public class PrintoutModel
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Sku { get; set; }
+        public int Quantity { get; set; }
+        public bool Is_Printed { get; set; }
+    }
+}
