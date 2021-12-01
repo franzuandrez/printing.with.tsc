03d1a15f19ae6a1b6ace79839afd89e9f7b7862f
franzua <edson.andrez@outlook.es>
2021-12-01 12:06:18 -0600

    Add loaders to DatagridView

--- Forms/FrmPendingOrders.Designer.cs
+++ Forms/FrmPendingOrders.Designer.cs
@@ -37,6 +37,8 @@
             this.btnImprimirTodo = new System.Windows.Forms.Button();
             this.btnImprimir = new System.Windows.Forms.Button();
             this.btnReimprimir = new System.Windows.Forms.Button();
+            this.pbOrders = new System.Windows.Forms.ProgressBar();
+            this.pbPrintouts = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridOrders)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridPrintouts)).BeginInit();
             this.SuspendLayout();
@@ -49,7 +51,7 @@
             // dataGridOrders
             // 
             this.dataGridOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridOrders.Location = new System.Drawing.Point(70, 122);
+            this.dataGridOrders.Location = new System.Drawing.Point(21, 122);
             this.dataGridOrders.Name = "dataGridOrders";
             this.dataGridOrders.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridOrders.Size = new System.Drawing.Size(467, 413);
@@ -61,19 +63,20 @@
             // dataGridPrintouts
             // 
             this.dataGridPrintouts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridPrintouts.Location = new System.Drawing.Point(561, 122);
+            this.dataGridPrintouts.Location = new System.Drawing.Point(511, 122);
             this.dataGridPrintouts.Name = "dataGridPrintouts";
             this.dataGridPrintouts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridPrintouts.Size = new System.Drawing.Size(507, 413);
+            this.dataGridPrintouts.Size = new System.Drawing.Size(661, 413);
             this.dataGridPrintouts.TabIndex = 2;
             this.dataGridPrintouts.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(67, 93);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 81);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(207, 13);
+            this.label1.Size = new System.Drawing.Size(461, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Listado de Ordenes Pendientes de ingreso";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -81,24 +84,26 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(558, 93);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(506, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.Size = new System.Drawing.Size(139, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Impresiones";
             // 
             // btnImprimirTodo
             // 
-            this.btnImprimirTodo.Location = new System.Drawing.Point(1074, 122);
+            this.btnImprimirTodo.Location = new System.Drawing.Point(1178, 122);
             this.btnImprimirTodo.Name = "btnImprimirTodo";
             this.btnImprimirTodo.Size = new System.Drawing.Size(97, 70);
             this.btnImprimirTodo.TabIndex = 5;
             this.btnImprimirTodo.Text = "Imprimir Todos los pendientes";
             this.btnImprimirTodo.UseVisualStyleBackColor = true;
+            this.btnImprimirTodo.Click += new System.EventHandler(this.btnImprimirTodo_Click);
             // 
             // btnImprimir
             // 
-            this.btnImprimir.Location = new System.Drawing.Point(1074, 211);
+            this.btnImprimir.Location = new System.Drawing.Point(1178, 218);
             this.btnImprimir.Name = "btnImprimir";
             this.btnImprimir.Size = new System.Drawing.Size(97, 74);
             this.btnImprimir.TabIndex = 6;
@@ -107,18 +112,35 @@
             // 
             // btnReimprimir
             // 
-            this.btnReimprimir.Location = new System.Drawing.Point(1074, 309);
+            this.btnReimprimir.Location = new System.Drawing.Point(1178, 312);
             this.btnReimprimir.Name = "btnReimprimir";
             this.btnReimprimir.Size = new System.Drawing.Size(97, 74);
             this.btnReimprimir.TabIndex = 7;
             this.btnReimprimir.Text = "Reimprimir";
             this.btnReimprimir.UseVisualStyleBackColor = true;
             // 
+            // pbOrders
+            // 
+            this.pbOrders.Location = new System.Drawing.Point(21, 282);
+            this.pbOrders.Name = "pbOrders";
+            this.pbOrders.Size = new System.Drawing.Size(467, 23);
+            this.pbOrders.TabIndex = 9;
+            // 
+            // pbPrintouts
+            // 
+            this.pbPrintouts.Location = new System.Drawing.Point(511, 281);
+            this.pbPrintouts.Name = "pbPrintouts";
+            this.pbPrintouts.Size = new System.Drawing.Size(661, 23);
+            this.pbPrintouts.TabIndex = 10;
+            this.pbPrintouts.Visible = false;
+            // 
             // FrmPendingOrders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1183, 547);
+            this.ClientSize = new System.Drawing.Size(1287, 677);
+            this.Controls.Add(this.pbPrintouts);
+            this.Controls.Add(this.pbOrders);
             this.Controls.Add(this.btnReimprimir);
             this.Controls.Add(this.btnImprimir);
             this.Controls.Add(this.btnImprimirTodo);
@@ -146,5 +168,7 @@
         private System.Windows.Forms.Button btnImprimirTodo;
         private System.Windows.Forms.Button btnImprimir;
         private System.Windows.Forms.Button btnReimprimir;
+        private System.Windows.Forms.ProgressBar pbOrders;
+        private System.Windows.Forms.ProgressBar pbPrintouts;
     }
 }
--- Forms/FrmPendingOrders.cs
+++ Forms/FrmPendingOrders.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 using TSCLIB_DLL_IN_C_Sharp.App;
 
@@ -13,17 +8,48 @@ namespace TSCLIB_DLL_IN_C_Sharp
 {
     public partial class FrmPendingOrders : Form
     {
+
+       
         public FrmPendingOrders()
         {
             InitializeComponent();
         }
 
+        private void showProgressBar(ProgressBar progressBar)
+        {
+            progressBar.Visible = true;
+            var rand = new Random();
+            int max = rand.Next(50, 80);
+
+            for (int i = 0; i < max; i ++)
+            {
+                progressBar.Value = i;
+            }
+           
+        }
+
+        private void hideProgressBar(ProgressBar progressBar)
+        {
+            
+
+            for (int i = 80; i <=100; i++)
+            {
+                progressBar.Value = i;
+            }
+          
+          progressBar.Visible = false;
+
+        }
         private async void FrmPendingOrders_Load(object sender, EventArgs e)
         {
+
             APIHelper.InitializeCliente();
+            showProgressBar(pbOrders);
             var orders = await PendingOrdersProcessor.LoadPendingOrders();
+            hideProgressBar(pbOrders);
             dataGridOrders.EditMode = DataGridViewEditMode.EditProgrammatically;
-            dataGridOrders.DataSource = orders;    
+            dataGridOrders.DataSource = orders;
+            
         }
 
       
@@ -56,15 +82,37 @@ namespace TSCLIB_DLL_IN_C_Sharp
             if (e.RowIndex >= 0)
             {
                 DataGridViewRow row = this.dataGridOrders.Rows[e.RowIndex];
-                Console.WriteLine(row.Cells[0].Value.ToString());
 
+                showProgressBar(pbPrintouts);
                 var printouts = await PrintoutsProcessor.LoadPrintouts(Int32.Parse(row.Cells[0].Value.ToString()));
                 dataGridPrintouts.EditMode = DataGridViewEditMode.EditProgrammatically;
                 dataGridPrintouts.DataSource = printouts;
+                hideProgressBar(pbPrintouts);
             }
         }
 
         private void dataGridOrders_ReadOnlyChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnImprimirTodo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private async void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+           
+        }
+
+        private void pcLoader_Click(object sender, EventArgs e)
         {
 
         }
