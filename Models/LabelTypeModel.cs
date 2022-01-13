f2020892a65eabfd940aa638ca6828d0ebf366c5
franzua <edson.andrez@outlook.es>
2022-01-13 15:05:38 -0600

    Add new attributes to LabelType Model

--- Models/LabelTypeModel.cs
+++ Models/LabelTypeModel.cs
@@ -9,16 +9,25 @@ namespace TSCLIB_DLL_IN_C_Sharp.Models
     public class LabelTypeModel
     {
         public int Id { get; set; }
-        public String Dimension { get; set; }
-        public String Unit { get; set; }
+        public String Name { get; set; }
+        public Double LabelWidth { get; set; }
+        public Double LabelHeight { get; set; }
+        public Double XValue { get; set; }
+        public Double YValue { get; set; }
+        public String Type { get; set; }
+        public Double CodeHeight { get; set; }
+        public Boolean Readable { get; set; }
+        public Double Rotation{ get; set; }
+        public Double Narrow{ get; set; }
+        public Double Wide{ get; set; }
+        public String Unit{ get; set; }
+        public Double XProductName{ get; set; }
+        public Double YProductName{ get; set; }
+        public Double HeightProductName { get; set; }
 
-        public String Name {
-        
-        get 
-            {
-                return Dimension + " / " + Unit;
-            } set { Name = Dimension + " / "+ Unit; }
-        }
+
+
+       
 
     }
 }
